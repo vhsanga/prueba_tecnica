01cafc8e92fb369118c3ad8b1b7221d232bfacfb
Victor Sanga <vic_vcp@hotmail.com>
2025-07-17 01:19:57 -0500

    Feature: F2. búsqueda de productos financieros

--- src/app/modules/productos-financieros/components/listado-productos/listado-productos.component.ts
+++ src/app/modules/productos-financieros/components/listado-productos/listado-productos.component.ts
@@ -9,22 +9,32 @@ import { ApiService } from '../../../../core/services/api.service';
 })
 export class ListadoProductosComponent implements OnInit {
   lProducts: any[] = [];
+  lProductsFiltered: any[] = [];
+  filter:string = '';
   constructor(private  apiService: ApiService){}
 
   ngOnInit(): void {
     this.apiService.doGet('bp/products', {}, (data: any) => {
       console.log("Productos obtenidos:", data);
       this.lProducts = data;
+      this.lProductsFiltered = data;
     });
   }
 
 
   getIniciales(nombre: string): string {
-  if (!nombre) return '';
-  const palabras = nombre.trim().toUpperCase().split(/\s+/);
-  const iniciales = palabras.slice(0, 2).map(p => p[0]).join('');
-  return iniciales;
-}
+    if (!nombre) return '';
+    const palabras = nombre.trim().toUpperCase().split(/\s+/);
+    const iniciales = palabras.slice(0, 2).map(p => p[0]).join('');
+    return iniciales;
+  }
+
+  searchItems(){
+    const filtroLower = this.filter.toLowerCase();
+    this.lProductsFiltered = this.lProducts.filter(p =>
+      p.name.toLowerCase().includes(filtroLower)
+    );
+  }
 
   editarProducto(producto:any){}
 
--- src/app/modules/productos-financieros/productos-financieros.module.ts
+++ src/app/modules/productos-financieros/productos-financieros.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { ProductosFinancierosRoutingModule } from './productos-financieros-routing.module';
 import { ListadoProductosComponent } from './components/listado-productos/listado-productos.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { ListadoProductosComponent } from './components/listado-productos/listad
   ],
   imports: [
     CommonModule,
-    ProductosFinancierosRoutingModule
+    ProductosFinancierosRoutingModule,
+    FormsModule
   ]
 })
 export class ProductosFinancierosModule { }
